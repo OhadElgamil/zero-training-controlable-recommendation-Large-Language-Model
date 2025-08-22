1c940ffaca548ea662aa5ee23fd4d6415976ada5
Ohad Elgamil <STAFF\ohad.elgamil@houdini>
2025-08-22 03:23:13 +0300

    added files

---
+++ dump_prompts.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer
+from data.data_interface import DInterface
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+#fix an issue, dont know why but stackoverflow said to do so and it worked...
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+#this should create the data module
+data_module = DInterface(
+    llm_tokenizer= tokenizer,
+    dataset="movielens_data",
+    batch_size = 8,
+    max_epochs=1,
+    num_workers=1,
+    prompt_path="/home/ohad.elgamil/LLaRA/prompt/movie.txt"
+)
+
+
+train_loader = data_module.train_dataloader()
+
+with open("/home/ohad.elgamil/LLaRA/prompt/train_prompts.txt", "w", encoding="utf-8") as f:
+    for i, batch in enumerate(train_loader):
+        texts = tokenizer.batch_decode(batch["tokens"]["input_ids"], skip_special_tokens=True)
+        for t in texts:
+            f.write(t.replace("\n", " ") + "\n")
+        if i >= 30: #stop after 20 iterations, can change to whatever
+            break 
